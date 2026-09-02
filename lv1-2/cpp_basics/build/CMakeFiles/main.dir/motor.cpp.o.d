fe145ddb334193187dda470e89d14006dc79e503
cowsjh <ef70024@gmail.com>
2026-09-02 18:02:46 +0900

    chore: stop tracking build artifacts

--- lv1-2/cpp_basics/build/CMakeFiles/main.dir/motor.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/main.dir/motor.cpp.o: \
- /home/pa2/git/physicalai-lv1-assignments/lv1-2/cpp_basics/motor.cpp \
- /usr/include/stdc-predef.h \
- /home/pa2/git/physicalai-lv1-assignments/lv1-2/cpp_basics/motor.hpp
